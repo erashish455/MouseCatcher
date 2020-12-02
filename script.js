ea49404a5fce36e37417b824048bc0ce49869867
ashish kumar kashyap <74764542+erashish455@users.noreply.github.com>
2020-12-02 12:25:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function randomsq() {
     square1.forEach(c => {
         c.classList.remove("mole")
     })
-    i = Math.floor(Math.random() * 6)
+    i = Math.floor(Math.random() * 9)
     counter = 0;
     square1[i].classList.add("mole")
     hitid = square1[i].id
